d9caa3b3441627e0f19e29998b026e132c5b9d8d
oyangchin <oyang0124@gmail.com>
2021-05-11 14:44:53 +0800

    加上變數跟分法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-pipes-v10';
+  name = '';
+  date = '';
+  height = '';
+  amount = '';
+  miles = '';
+
+  onNameChange(value){
+    console.log('name : ', value);
+    this.name = value;
+  }
+
+  onDateChange(value){
+    console.log('date : ', value);
+    this.date = value;
+  }
+
+  onHeightChange(value){
+    console.log('height : ', value);
+    this.height = value;
+  }
+
+  onAmountChange(value){
+    console.log('amount : ', value);
+    this.amount = value;
+  }
+
+  onMilesChange(value){
+    console.log('miles : ', value);
+    this.miles = value;
+  }
+
+
+
 }
