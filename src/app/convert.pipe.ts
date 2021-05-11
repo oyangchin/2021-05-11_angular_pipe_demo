8e90981329b9a458d8e0e8bdf2f83a4b06d3efcc
oyangchin <oyang0124@gmail.com>
2021-05-11 15:03:49 +0800

    使用 pipe 跟 客製化的 convert pipe

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -1,12 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'convert'
+  name: 'convert',
 })
 export class ConvertPipe implements PipeTransform {
+  transform(value: any, targetUnits: String): unknown {
+    if (value == '') {
+      return null;
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.60934;
+      case 'm':
+        return value * 1.60934 * 1000;
+      case 'cm':
+        return value * 1.60934 * 1000 * 1000;
+      default:
+        throw new Error('Target unit not supported');
+    }
   }
-
 }
